3a6f50aa43c15a59c96e939aea857b20bf5163b3
Pavel Savchenko <asfaltboy@gmail.com>
2012-06-07 09:00:56 +0300

    update version in setup.py, also need to upload latest version to PyPi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 setup(name='django-pure-pagination',
-      version='0.1',
+      version='0.2',
       author='James Pacileo',
       long_description = open('README.rst').read(),
       license='BSD',
